e68f3b075b29592805caf899ec5cd73f14a42a2b
GMALLECOM <108859723+GMALLECOM@users.noreply.github.com>
2023-09-04 16:40:36 +0300

    Create JwtMiddleware.cs

---
+++ Middleware/JwtMiddleware.cs
@@ -0,0 +1,52 @@
+using System;
+using System.IdentityModel.Tokens.Jwt;
+using System.Linq;
+using Microsoft.AspNetCore.Http;
+using Microsoft.IdentityModel.Tokens;
+
+public class JwtMiddleware
+{
+    private readonly RequestDelegate _next;
+
+    public JwtMiddleware(RequestDelegate next)
+    {
+        _next = next;
+    }
+
+    public async Task InvokeAsync(HttpContext context)
+    {
+        var token = context.Request.Headers["Authorization"].FirstOrDefault()?.Split(" ").Last();
+
+        if (token != null)
+            AttachUserToContext(context, token);
+
+        await _next(context);
+    }
+
+    private void AttachUserToContext(HttpContext context, string token)
+    {
+        try
+        {
+            var tokenHandler = new JwtSecurityTokenHandler();
+            var key = Encoding.ASCII.GetBytes("YourSecretKeyHere"); // Replace with your secret key
+            tokenHandler.ValidateToken(token, new TokenValidationParameters
+            {
+                ValidateIssuerSigningKey = true,
+                IssuerSigningKey = new SymmetricSecurityKey(key),
+                ValidateIssuer = false,
+                ValidateAudience = false,
+                ClockSkew = TimeSpan.Zero
+            }, out SecurityToken validatedToken);
+
+            var jwtToken = (JwtSecurityToken)validatedToken;
+            var userId = int.Parse(jwtToken.Claims.First(x => x.Type == "id").Value);
+
+            // Attach user to context here
+            context.Items["User"] = userId;
+        }
+        catch
+        {
+            // Exception handling logic here, for instance, you can log the error
+        }
+    }
+}
